9219e30318dcc84c5f4a4e7bf1afcb5a3449f197
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-04-19 04:35:04 -0400

    Add files via upload

--- Preprocessing/geocoding.py
+++ Preprocessing/geocoding.py
@@ -21,13 +21,21 @@ from geopy import geocoders
 from geopy.geocoders import GoogleV3
 
 
-# In[15]:
+# In[25]:
 
 
-address = 'Times Square, NY'
+address = 'CENTRAL AVE'
 YOUR_API_KEY =  "AIzaSyD2v_0UftOba5BQyt5WY19cjAFdiu9S7FQ"
 g = GoogleV3(api_key=YOUR_API_KEY)
 location = g.geocode(address, timeout=15)
+if(location == None):
+    location = g.geocode(address + " Chicago")
 print(location.latitude)
 print(location.longitude)
+
+
+# In[ ]:
+
+
+
 
