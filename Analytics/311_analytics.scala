791322af469cb1969f5fbc93e78cd7fb1ea9f9bf
Divya Juneja <48163677+dj1322@users.noreply.github.com>
2019-04-19 23:16:26 -0400

    Update 311_analytics.scala

--- Analytics/311_analytics.scala
+++ Analytics/311_analytics.scala
@@ -1,12 +1,7 @@
-﻿df.filter(($"IS THE BUILDING CURRENTLY VACANT OR OCCUPIED?"==="Vacant") && ($"ANY PEOPLE USING PROPERTY? (HOMELESS, CHILDEN, GANGS)"==="true")).count()
-
-
-
-
+// 1. Number of buildings vacant and occupied by someone signifies probability of more crime. Value is analyzed in our case.
+df.filter(($"IS THE BUILDING CURRENTLY VACANT OR OCCUPIED?"==="Vacant") && ($"ANY PEOPLE USING PROPERTY? (HOMELESS, CHILDEN, GANGS)"==="true")).count()
 res23: Long = 25506
 
-
+// 2. Number of buildings vacant due to fire signifies risk. Value is analyzed in our case.
 df.filter(($"IS THE BUILDING VACANT DUE TO FIRE?"==="true")).count()
-
-
 res27: Long = 4541
