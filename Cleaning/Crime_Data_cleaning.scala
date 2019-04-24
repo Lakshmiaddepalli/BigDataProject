6dfd25af91aaac4ba13357a410ae227a5b3fce1d
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-04-23 22:43:17 -0400

    Update Crime_Data_cleaning.scala

--- Cleaning/Crime_Data_cleaning.scala
+++ Cleaning/Crime_Data_cleaning.scala
@@ -1,7 +1,7 @@
 spark-shell --packages com.databricks:spark-csv_2.10:1.5.0
 import org.apache.spark.sql.SQLContext
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-var df = sqlContext.read.format("csv").option("header", "true").load("hdfs:///user/sla410/crimedatabigdataproject/Crimes_-_2001_to_present.csv")
+var df = sqlContext.read.format("csv").option("header", "true").load("hdfs:///user/sla410/crimedatabigdataproject/crimesfinal.csv")
 df = df.withColumn("Date_of_Crime", split($"Date", " ")(0))
 df = df.withColumn("Time", split($"Date", " ")(1))
 df = df.drop("Date")
@@ -30,22 +30,20 @@ val todate = udf[String,String](dateclean)
 df = df.withColumn("Date_of_Crime", todate($"Date_of_Crime"))
 df = df.withColumn("Date_of_Crime", (col("Date_of_Crime").cast("date")))
 df = df.filter($"Date_of_Crime".between("2010-01-01", "2011-12-31"))
-df = df.filter($"Date_of_Crime".between("2008-01-01", "2011-12-31"))
-
 
 df.printSchema()
 df.filter(df("ID").isNull || df("ID") === "" || df("ID").isNaN).count() //gave 0
 df.filter(df("Case Number").isNull || df("Case Number") === "" || df("Case Number").isNaN).count() //gave 0 
 df.filter(df("Block").isNull || df("Block") === "" || df("Block").isNaN).count() //0
-df.filter(df("IUCR").isNull || df("Block") === "" || df("IUCR").isNaN).count() //0
+df.filter(df("IUCR").isNull || df("IUCR") === "" || df("IUCR").isNaN).count() //0
 df.filter(df("Primary Type").isNull || df("Primary Type") === "" || df("Primary Type").isNaN).count() //0
 df.filter(df("Description").isNull || df("Description") === "" || df("Description").isNaN).count() //0
-df.filter(df("Location Description").isNull || df("Location Description") === "" || df("Location Description").isNaN).count() //17
+df.filter(df("Location Description").isNull || df("Location Description") === "" || df("Location Description").isNaN).count() //313
 df.filter(df("Arrest").isNull || df("Arrest") === "" || df("Arrest").isNaN).count() //0
 df.filter(df("Beat").isNull || df("Beat") === "" || df("Beat").isNaN).count() //0
-df.filter(df("District").isNull || df("District") === "" || df("District").isNaN).count() //47
-df.filter(df("Ward").isNull || df("Ward") === "" || df("Ward").isNaN).count() 
-df.filter(df("Community Area").isNull || df("Community Area") === "" || df("Community Area").isNaN).count() //8
+df.filter(df("District").isNull || df("District") === "" || df("District").isNaN).count() //0
+df.filter(df("Ward").isNull || df("Ward") === "" || df("Ward").isNaN).count() //32
+df.filter(df("Community Area").isNull || df("Community Area") === "" || df("Community Area").isNaN).count() //0
 df.filter(df("FBI Code").isNull || df("FBI Code") === "" || df("FBI Code").isNaN).count() //0
 df.filter(df("X Coordinate").isNull || df("X Coordinate") === "" || df("X Coordinate").isNaN).count() //850
 df.filter(df("Y Coordinate").isNull || df("Y Coordinate") === "" || df("Y Coordinate").isNaN).count() //850
