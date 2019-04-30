5d2487d4eeb502c3c4e62176ee7e82cd811f2830
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-04-30 12:41:36 -0400

    Update Logistic_regression.scala

--- DL_Algorithm/Logistic_regression.scala
+++ DL_Algorithm/Logistic_regression.scala
@@ -1,4 +1,20 @@
 spark-shell --packages com.databricks:spark-csv_2.10:1.5.0
+
+import scala.reflect.runtime.universe
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.feature.Bucketizer
+import org.apache.spark.ml.feature.Normalizer
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.functions.mean
+
+
 val sqlc = new SQLContext(sc)
 var dfcrime = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/crimedl1.csv").cache()
 var dfhealth = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/publichealth.csv").cache()
