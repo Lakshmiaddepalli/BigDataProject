dd7d58b20677e638984f381a640016da1f7920cc
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-04-30 11:49:41 -0400

    Update Logistic_regression.scala

--- DL_Algorithm/Logistic_regression.scala
+++ DL_Algorithm/Logistic_regression.scala
@@ -1,1 +1,4 @@
-.option("inferSchema", "true")
+val sqlc = new SQLContext(sc)
+var dfcrime = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/crimedl1.csv")
+var dfhealth = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/publichealth.csv")
+var dfsocioeconomiccensus = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/socioeconomicfactors2.csv")
