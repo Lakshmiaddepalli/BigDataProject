b4080d12404fb3bf4357b870b7dae294ae4daaf8
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-04-30 12:25:17 -0400

    Update Logistic_regression.scala

--- DL_Algorithm/Logistic_regression.scala
+++ DL_Algorithm/Logistic_regression.scala
@@ -3,3 +3,7 @@ val sqlc = new SQLContext(sc)
 var dfcrime = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/crimedl1.csv").cache()
 var dfhealth = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/publichealth.csv").cache()
 var dfsocioeconomiccensus = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/socioeconomicfactors2.csv").cache()
+
+dfcrime.registerTempTable("crime")
+dfhealth.registerTempTable("health")
+dfsocioeconomiccensus.registerTempTable("socioeconomiccensus")
