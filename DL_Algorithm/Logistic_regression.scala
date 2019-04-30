c4cdd39dc6fef5ba1e69d96543d30a74e1d79c74
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-04-30 11:52:40 -0400

    Update Logistic_regression.scala

--- DL_Algorithm/Logistic_regression.scala
+++ DL_Algorithm/Logistic_regression.scala
@@ -1,5 +1,5 @@
 spark-shell --packages com.databricks:spark-csv_2.10:1.5.0
 val sqlc = new SQLContext(sc)
-var dfcrime = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/crimedl1.csv")
-var dfhealth = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/publichealth.csv")
-var dfsocioeconomiccensus = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/socioeconomicfactors2.csv")
+var dfcrime = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/crimedl1.csv").cache()
+var dfhealth = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/publichealth.csv").cache()
+var dfsocioeconomiccensus = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/socioeconomicfactors2.csv").cache()
