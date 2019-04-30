054c902122ebdcde8ba71555648e6301d7763ffb
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-04-30 12:26:53 -0400

    Update Logistic_regression.scala

--- DL_Algorithm/Logistic_regression.scala
+++ DL_Algorithm/Logistic_regression.scala
@@ -7,3 +7,33 @@ var dfsocioeconomiccensus = sqlc.read.format("com.databricks.spark.csv").option(
 dfcrime.registerTempTable("crime")
 dfhealth.registerTempTable("health")
 dfsocioeconomiccensus.registerTempTable("socioeconomiccensus")
+
+val crimefactors = sqlc.sql("""SELECT
+        crime.Year,crime.Month,crime.Day,crime.Time,
+        crime.IUCR, crime.Primary_Type,crime.Description,crime.Location_Description,
+        crime.Community_Area, crime.Arrest,crime.FBI_Code,crime.Latitude,crime.Longitude,
+        socioeconomiccensus.PERCENT_OF_HOUSING_CROWDED, 
+        socioeconomiccensus.PERCENT_HOUSEHOLDS_BELOW_POVERTY,
+        socioeconomiccensus.PERCENT_AGED_16+__UNEMPLOYED, 
+        socioeconomiccensus.PERCENT_AGED_25+__WITHOUT_HIGH_SCHOOL_DIPLOMA,
+        socioeconomiccensus.PERCENT_AGED_UNDER_18_OR_OVER_64,
+        socioeconomiccensus.PER_CAPITA_INCOME, 
+        socioeconomiccensus.HARDSHIP_INDEX,
+        health.Birth_Rate,
+        health.General_Fertility_Rate,
+        health.Low_Birth_Weight,
+        health.Prenatal_Care_Beginning_in_First_Trimester,
+        health.Preterm_Births,health.Teen_Birth_Rate,health.Assault,
+        health.Breast_cancer_in_females,health.Cancer,health.Colorectal_Cancer,
+        health.Diabetes_related,health.Firearm_related,
+        health.Infant_Mortality_Rate,health.Lung_Cancer,
+        health.Prostate_Cancer_in_Males,health.Stroke,
+        health.Childhood_Blood_Lead_Level_Screening,
+        health.Childhood_Lead_Poisoning,
+        health.Gonorrhea_in_Females,
+        health.Gonorrhea_in_Males,
+        health.Tuberculosis
+        FROM crime JOIN socioeconomiccensus 
+        ON crime.Community_Area = socioeconomiccensus.Community_Area
+        JOIN health
+        ON crime.Community_Area = health.Community_Area""".stripMargin)
