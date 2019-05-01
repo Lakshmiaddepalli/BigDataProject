c87047a68687154bbb75694d5c9e98f1c9c81d1d
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-05-01 00:57:17 -0400

    Update Logistic_regression.scala

--- DL_Algorithm/Logistic_regression.scala
+++ DL_Algorithm/Logistic_regression.scala
@@ -14,11 +14,24 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.functions.mean
 
-
-val sqlc = new SQLContext(sc)
-
 case class Crime(Year: Double, Month: Double, Day: Double, Time: String, IUCR: String, Primary_Type: String,Description: String, 
                  Location_Description: String,Community_Area: Double, FBI_Code: String, Latitude: Double, Longitude: Double)
+
+object Project {
+  
+def main (args: Array[String]) {
+        
+        if (args.length < 12) {
+            System.err.println("Please Enter all the inputs. . . .")
+            System.exit(1)
+        }
+        
+        
+        System.out.println("Starting Spark SQL Context...")
+        val sqlc = new SQLContext(sc) 
+        import sqlContext._  
+        import sqlContext.implicits._ 
+        val directory = args(0)
 
 var dfcrime = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/crimedl1.csv").cache()
 var dfhealth = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/publichealth.csv").cache()
@@ -88,6 +101,12 @@ val gnmalesInd = new StringIndexer().setInputCol("Gonorrhea_in_Males").setOutput
 //all the correlation values between arrest and other features lied between -0.01 to 0.01 and hence are highly inependent of each other.
 valcrime.select(corr($"Arrest",$"Year")).show()
 
+val crimetest = Seq(Crime(args(0).toDouble, args(1).toDouble, args(2).toDouble, args(3),args(4), args(5), args(6), args(7), args(8).toDouble, args(9),args(10).toDouble,args(11).toDouble))
+val crimetestdata = sqlc.sparkContext.parallelize(Seq(crimetestdata)).toDF
+val joinedhealthdata = health.join(crimetestdata).where('Community_Area === 'Community_Area)
+val joinedsocioeconomichealth = socioeconomiccensus.join(joinedhealthdata).where('Community_Area === 'Community_Area)
+  
+  
 val assembler = new VectorAssembler().setInputCols(Array("Year", "Month", "Day", "TimeIndex","IUCRIndex", "PrimaryTypeIndex","DescriptionIndex","LocationDescriptionIndex","Community_Area","FBICodeIndex","Latitude","Longitude","PERCENT_OF_HOUSING_CROWDED","PERCENT_HOUSEHOLDS_BELOW_POVERTY","PERCENT_AGED_16_UNEMPLOYED","PERCENT_AGED_25_WITHOUT_HIGH_SCHOOL_DIPLOMA","PERCENT_AGED_UNDER_18_OR_OVER_64","PER_CAPITA_INCOME","HARDSHIP_INDEX","Birth_Rate","General_Fertility_Rate","Low_Birth_Weight","Prenatal_Care_Beginning_in_First_Trimester","Preterm_Births","Teen_Birth_Rate","Assault","Breast_cancer_in_females","Cancer","Colorectal_Cancer","Diabetes_related","Firearm_related","Infant_Mortality_Rate","Lung_Cancer","Prostate_Cancer_in_Males","Stroke","Childhood_Blood_Lead_Level_Screening","Childhood_Lead_Poisoning","Gonorrhea_in_Females","GonorrheainMalesIndex","Tuberculosis")).setOutputCol("features_temp")
 val normalizer = new Normalizer().setInputCol("features_temp").setOutputCol("features").setP(1.0)
 val lr = new LogisticRegression().setMaxIter(100000)
@@ -107,3 +126,5 @@ val predictionAndLabels = result.map { row =>
 val metrics = new BinaryClassificationMetrics(predictionAndLabels)
 println("Area under ROC = " + metrics.areaUnderROC()) //Area under ROC = 0.5393021885782492
 model = pipeline.fit(train_data)
+}
+}
