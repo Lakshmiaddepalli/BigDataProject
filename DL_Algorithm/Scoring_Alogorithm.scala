18036d08e3a2a5bf4862ff8df2d85a34514d85a5
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-05-03 10:24:49 -0400

    Update Scoring_Alogorithm.scala

--- DL_Algorithm/Scoring_Alogorithm.scala
+++ DL_Algorithm/Scoring_Alogorithm.scala
@@ -10,3 +10,7 @@ import sqlContext.implicits._
 var dfcrime = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/crimedl1.csv").cache()
 var Communityareacount =  dfcrime.groupBy($"Community_Area").count().orderBy(desc("count"))
 Communityareacount.write.format("csv").option("header", "true").save("hdfs:///user/sla410/crimedatabigdataproject/crimecount.csv")
+
+var dfsexoffenders = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/SexOffendersfinal.csv").cache()
+var sexoffenderscount =  dfsexoffenders.groupBy($"Community Area Number").count().orderBy(desc("count"))
+sexoffenderscount.write.format("csv").option("header", "true").save("hdfs:///user/sla410/crimedatabigdataproject/sexoffenderscount.csv")
