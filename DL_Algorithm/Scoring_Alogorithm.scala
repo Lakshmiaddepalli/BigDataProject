2647a8d1f0eb47faacfc44cafd4393035544fe92
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-05-03 10:39:47 -0400

    Update Scoring_Alogorithm.scala

--- DL_Algorithm/Scoring_Alogorithm.scala
+++ DL_Algorithm/Scoring_Alogorithm.scala
@@ -17,4 +17,4 @@ sexoffenderscount.write.format("csv").option("header", "true").save("hdfs:///use
 
 
 var dfsocioeconomiccensus = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/socioeconomicfactors3.csv").cache()
-var hardshipcount = dfsocioeconomiccensus.select
+var hardshipcount = dfsocioeconomiccensus.select("")
