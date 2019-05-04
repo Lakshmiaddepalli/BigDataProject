cad06ded9f45b6ca81d042ab192646d3eb163499
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-05-03 23:48:02 -0400

    Update Scoring_Alogorithm.scala

--- DL_Algorithm/Scoring_Alogorithm.scala
+++ DL_Algorithm/Scoring_Alogorithm.scala
@@ -3,13 +3,19 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SQLContext
+import scala.collection.mutable.ArrayBuffer
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.clustering.KMeans
 val sqlc = new SQLContext(sc) 
 import sqlc._
 import sqlc.implicits._
 
 var dfcrime = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/crimedl1.csv").cache()
-var Communityareacount =  dfcrime.groupBy($"Community_Area").count().orderBy(desc("count"))
-Communityareacount.write.format("csv").option("header", "true").save("hdfs:///user/sla410/crimedatabigdataproject/crimecount.csv")
+var dfcrimefinal = dfcrime.filter(dfcrime("Primary_Type") === "OTHER OFFENSE" || dfcrime("Primary_Type") === "PUBLIC INDECENCY" || dfcrime("Primary_Type") === "DECEPTIVE PRACTICE" || dfcrime("Primary_Type") === "OBSCENITY" || dfcrime("Primary_Type") === "MOTOR VEHICLE THEFT" || dfcrime("Primary_Type") === "CRIMINAL DAMAGE" ||dfcrime("Primary_Type") === "GAMBLING" || dfcrime("Primary_Type") === "LIQUOR LAW VIOLATION" || dfcrime("Primary_Type") === "INTIMIDATION" || dfcrime("Primary_Type") === "NON-CRIMINAL" || dfcrime("Primary_Type") === "CRIMINAL TRESPASS" || dfcrime("Primary_Type") === "PUBLIC PEACE VIOLATION")
+var crimerdd = dfcrimefinal.rdd
+var crimelocations = crimerdd.map(x => Vectors.dense(x.getDouble(11),x.getDouble(12)))
+//var Communityareacount =  dfcrime.groupBy($"Community_Area").count().orderBy(desc("count"))
+//Communityareacount.write.format("csv").option("header", "true").save("hdfs:///user/sla410/crimedatabigdataproject/crimecount.csv")
 
 var dfsexoffenders = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/SexOffendersfinal.csv").cache()
 var sexoffenderscount =  dfsexoffenders.groupBy($"Community Area Number").count().orderBy(desc("count"))
