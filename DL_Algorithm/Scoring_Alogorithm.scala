1ed697a7fdb76a0b94efcb09b24e27e0d84624ee
Lakshmi Addepalli <lakshi.ana@gmail.com>
2019-05-04 05:10:20 -0400

    Update Scoring_Alogorithm.scala

--- DL_Algorithm/Scoring_Alogorithm.scala
+++ DL_Algorithm/Scoring_Alogorithm.scala
@@ -3,19 +3,33 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SQLContext
-import scala.collection.mutable.ArrayBuffer
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.clustering.KMeans
+
 val sqlc = new SQLContext(sc) 
 import sqlc._
 import sqlc.implicits._
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.clustering.KMeans
+import scala.collection.mutable.ArrayBuffer
+
 
 var dfcrime = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/crimedl1.csv").cache()
-var dfcrimefinal = dfcrime.filter(dfcrime("Primary_Type") === "OTHER OFFENSE" || dfcrime("Primary_Type") === "PUBLIC INDECENCY" || dfcrime("Primary_Type") === "DECEPTIVE PRACTICE" || dfcrime("Primary_Type") === "OBSCENITY" || dfcrime("Primary_Type") === "MOTOR VEHICLE THEFT" || dfcrime("Primary_Type") === "CRIMINAL DAMAGE" ||dfcrime("Primary_Type") === "GAMBLING" || dfcrime("Primary_Type") === "LIQUOR LAW VIOLATION" || dfcrime("Primary_Type") === "INTIMIDATION" || dfcrime("Primary_Type") === "NON-CRIMINAL" || dfcrime("Primary_Type") === "CRIMINAL TRESPASS" || dfcrime("Primary_Type") === "PUBLIC PEACE VIOLATION")
-var crimerdd = dfcrimefinal.rdd
-var crimelocations = crimerdd.map(x => Vectors.dense(x.getDouble(11),x.getDouble(12)))
+val crimerdd = dfcrime.rdd
+val crimelocations = crimerdd.map(x => Vectors.dense(x.getDouble(11),x.getDouble(12)))
+
 //var Communityareacount =  dfcrime.groupBy($"Community_Area").count().orderBy(desc("count"))
+val crimemodel = KMeans.train(crimelocations, 100, 20)
+var crimemapping = crimeModel.predict(crimelocations).map(r => (r, 1)).reduceByKey(_ + _).map(r => r._2)
+
+//crimemapping.collect.foreach(println)
+var joinval = crimeModel.clusterCenters.zip(crimemapping.collect())
+var crimepoints = sc.parallelize(joinval)
+
+//crimepoints.collect().foreach(println)
+var crimepointsmap = crimepoints.map( c => c._1(0).toString + "," + c._1(1).toString + "," + c._2)
+crimepointsmap.coalesce(1).saveAsTextFile("hdfs:///user/sla410/crimedatabigdataproject/crimecluster1.csv")
+//crimepointsmap.toDF.coalesce(1).write.format("csv").option("header", "true").save("hdfs:///user/sla410/crimedatabigdataproject/crimecluster.csv")
 //Communityareacount.write.format("csv").option("header", "true").save("hdfs:///user/sla410/crimedatabigdataproject/crimecount.csv")
+
 
 var dfsexoffenders = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/SexOffendersfinal.csv").cache()
 var sexoffenderscount =  dfsexoffenders.groupBy($"Community Area Number").count().orderBy(desc("count"))
@@ -28,5 +42,10 @@ hardshipcount.write.format("csv").option("header", "true").save("hdfs:///user/sl
 
 
 var dfaffordablehousing = sqlc.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("hdfs:///user/sla410/crimedatabigdataproject/Affordable_Rental_Housing_Developments.csv").cache()
-var affordhouse = dfaffordablehousing.groupBy("Community Area Number").agg(sum("Units"))
+var affordhouse = dfaffordablehousing.rdd
+var affordhouselocations = affordhouse.map(x => Vectors.dense(x.getDouble(12),x.getDouble(13)))
+//var affordhouse = dfaffordablehousing.groupBy("Community Area Number").agg(sum("Units"))
+
+val affordinghousesmodel = KMeans.train(affordhouselocations, 100, 20)
 affordhouse.write.format("csv").option("header", "true").save("hdfs:///user/sla410/crimedatabigdataproject/affordhousecountcommunitywise.csv")
+
